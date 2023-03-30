eb2375c0ae92914414000b73dc1fc70cf7e6cbc2
Omar shakib <omarshakib1100@gmail.com>
2023-03-30 16:23:18 +0600

    header

---
+++ src/component/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shopContainer'>
+            <div className='productContainer'>
+                <h2>Product coming here</h2>
+            </div>
+            <div className='cardContainer'>
+                <h4>Order Summery</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
